35ab9d0b7d42bf96790f75f95ae6362eaf0ff392
sjllls <2012ji123@gmail.com>
2021-04-17 20:50:34 +0800

    akari: Build without vendor partition

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,17 +12,11 @@ DEVICE_PATH := device/sony/akari
 # Assert
 TARGET_OTA_ASSERT_DEVICE := H8266,H8296,H8216,akari,akari_dual
 
-# Crypto
-TARGET_HW_DISK_ENCRYPTION := true
-
 # Display
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
 TARGET_KERNEL_CONFIG := tama_akari_defconfig
-
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Partitions
 # Reserve space for data encryption (44712771584-16384)
@@ -30,7 +24,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 44712755200
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Treble
 BOARD_VNDK_RUNTIME_DISABLE := true
